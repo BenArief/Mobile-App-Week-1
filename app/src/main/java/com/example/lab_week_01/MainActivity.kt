f5397a59fc1ef92cb794b00c02d4c39565fec22a
BenArief <benarief29@gmail.com>
2025-09-03 23:06:32 +0700

    done

--- app/src/main/java/com/example/lab_week_01/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_01/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.lab_week_01
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val nameInput = findViewById<TextInputEditText>(R.id.name_input)
+        val emailInput = findViewById<TextInputEditText>(R.id.email_input)
+        val submitButton = findViewById<Button>(R.id.submit_button)
+        val resultText = findViewById<TextView>(R.id.result_text)
+
+        submitButton.setOnClickListener {
+            val name = nameInput.text.toString()
+            val studentNumber = emailInput.text.toString()
+
+            resultText.text = "Hello my name is: $name\nStudent Number: $studentNumber"
         }
     }
 }
